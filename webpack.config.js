6de9cc01b5d47ead40614c95a05f6752c7804da2
Jeff <61642756+garber34@users.noreply.github.com>
2021-09-10 16:12:21 -0400

    minify the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,49 @@
 const path = require("path");
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
+   ///// using the terser plugin
+   optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          ecma: 2016,
+          parse: {},
+          compress: {
+            ecma:2016,
+            toplevel:true,
+            arguments:true,
+            booleans_as_integers:true,
+            drop_console:true,
+            hoist_funs:true,
+            keep_fargs:false,
+            passes:3,
+            pure_getters:true,
+            unsafe:true,
+            unsafe_Function:true,
+            unsafe_arrows:true,
+            unsafe_comps:true,
+            unsafe_math:true,
+            unsafe_methods:true,
+            unsafe_proto:true,
+            unsafe_regexp:true,
+            unsafe_symbols:true,
+            unsafe_undefined:true
+          },
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          module: false,
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_classnames: undefined,
+          keep_fnames: false,
+          safari10: false,
+        },
+      }),
+    ],
+  },
   entry: "./src/index.ts",
   module: {
     rules: [
