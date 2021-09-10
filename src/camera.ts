789e9ebec0e931dfbf81a74c70af193708d42a30
Adam Reeves <aar2222@gmail.com>
2021-09-10 14:44:54 -0500

    move camera freely

--- src/camera.ts
+++ src/camera.ts
@@ -5,14 +5,13 @@ const { canvas, gl, program } = initialize;
 const { zoom } = constants;
 
 export class Camera {
-  eye: DOMPoint;
-  target: DOMPoint;
+  viewMatrix: DOMMatrix;
   projMatrix: DOMMatrix;
   cameraGL: WebGLUniformLocation;
-  constructor(eye: DOMPoint, target: DOMPoint) {
-    this.eye = eye;
-    this.target = target;
+  constructor() {
     this.cameraGL = gl.getUniformLocation(program, "camera");
+    this.viewMatrix = new DOMMatrix();
+    this.viewMatrix.translateSelf(0, 3, 13);
     this.projMatrix = this.perspective(
       zoom,
       canvas.width / canvas.height,
@@ -30,72 +29,14 @@ export class Camera {
       0, 0, (2 * near * far) / (near - far), 0
     ]);
   };
-  lookAt = (
-    cameraX: number,
-    cameraY: number,
-    cameraZ: number,
-    targetX: number,
-    targetY: number,
-    targetZ: number,
-    upX = 0,
-    upY = 1,
-    upZ = 0
-  ) => {
-    let fx, fy, fz, rlf, sx, sy, sz, rls, ux, uy, uz;
-    fx = targetX - cameraX;
-    fy = targetY - cameraY;
-    fz = targetZ - cameraZ;
-    rlf = 1 / Math.hypot(fx, fy, fz);
-    fx *= rlf;
-    fy *= rlf;
-    fz *= rlf;
-    sx = fy * upZ - fz * upY;
-    sy = fz * upX - fx * upZ;
-    sz = fx * upY - fy * upX;
-    rls = 1 / Math.hypot(sx, sy, sz);
-    sx *= rls;
-    sy *= rls;
-    sz *= rls;
-    ux = sy * fz - sz * fy;
-    uy = sz * fx - sx * fz;
-    uz = sx * fy - sy * fx;
-    // prettier-ignore
-    const ret = new DOMMatrix([
-      sx, ux, -fx, 0,
-      sy, uy, -fy, 0,
-      sz, uz, -fz, 0,
-      0, 0, 0, 1,
-    ]);
-    return ret.translateSelf(-cameraX, -cameraY, -cameraZ);
-  };
   view() {
-    const { eye, target } = this;
-    const viewMatrix = this.lookAt(
-      eye.x,
-      eye.y,
-      eye.z,
-      target.x,
-      target.y,
-      target.z
-    );
-    const cameraMatrix = this.projMatrix.multiply(viewMatrix);
+    const cameraMatrix = this.projMatrix.multiply(this.viewMatrix.inverse());
     gl.uniformMatrix4fv(this.cameraGL, false, cameraMatrix.toFloat32Array());
   }
-  rotateAroundEye(x: number, y: number) {
-    const t = new DOMMatrix();
-    t.translateSelf(-this.eye.x, -this.eye.y, -this.eye.z);
-    this.target = this.target.matrixTransform(t);
-    // const matrix = new DOMMatrix(`rotate3d(${0 * y},${0 * x},${y},1deg)`);
-    const matrix = new DOMMatrix();
-    matrix.rotateSelf(y * 20, x * 20, 0);
-    this.target = this.target.matrixTransform(matrix);
-    t.invertSelf();
-    this.target = this.target.matrixTransform(t);
+  rotate(x: number, y: number, z: number) {
+    this.viewMatrix.rotateSelf(y, x, z);
   }
   move(x = 0, y = 0, z = 0) {
-    const t = new DOMMatrix();
-    t.translateSelf(x, y, z);
-    this.target = this.target.matrixTransform(t);
-    this.eye = this.eye.matrixTransform(t);
+    this.viewMatrix.translateSelf(x, y, z);
   }
 }
